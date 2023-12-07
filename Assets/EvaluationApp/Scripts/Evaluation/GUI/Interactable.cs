b45fd9368f3e82edc07031e248f547c8ec5d3501
IXLAB <hannes.reinhold2001@gmail.com>
2023-12-07 17:15:01 +0100

    Add more UI

--- Assets/EvaluationApp/Scripts/Evaluation/GUI/Interactable.cs
+++ Assets/EvaluationApp/Scripts/Evaluation/GUI/Interactable.cs
@@ -9,15 +9,15 @@ public class Interactable : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     public bool playClickSound = false;
     public FMODUnity.EventReference clickSound;
-    public float clickVibration = 0.1f;
+    public float clickVibration = 0.2f;
 
     public bool playEnterSound = false;
     public FMODUnity.EventReference enterSound;
-    public float enterVibration = 0.1f;
+    public float enterVibration = 0.05f;
 
     public bool playExitSound = false;
     public FMODUnity.EventReference exitSound;
-    public float exitVibration = 0.1f;
+    public float exitVibration = 0.05f;
 
 
     void Awake()
@@ -29,19 +29,19 @@ public class Interactable : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public void OnPointerClick(PointerEventData eventData)
     {
         if (playClickSound) FMODUnity.RuntimeManager.PlayOneShot(clickSound, transform.position);
-        Vib(0.1f, clickVibration);
+        Vib(0, clickVibration);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (playEnterSound) FMODUnity.RuntimeManager.PlayOneShot(enterSound, transform.position);
-        Vib(0.1f, enterVibration);
+        Vib(0, enterVibration);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         if (playExitSound) FMODUnity.RuntimeManager.PlayOneShot(exitSound, transform.position);
-        Vib(0.1f, exitVibration);
+        Vib(0, exitVibration);
     }
 
     public void Vib(float start, float end)
